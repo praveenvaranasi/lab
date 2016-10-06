cfd668933d0ec4f8235f9ae6ee252a5ce3e03afb
Praveen <praveenb.varanasi@outlook.com>
2016-10-06 18:25:50 +0530

    Added utilities to use in daily life

---
+++ Selenium/utilities/src/dailylife/utilities/Utilities.java
@@ -0,0 +1,34 @@
+package dailylife.utilities;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import dailylife.utilities.banking.GetBalanceFromBank;
+import java.io.IOException;
+
+
+public class Utilities 
+{
+	public static void main(String args[]) throws IOException, InterruptedException
+	{
+		int option;
+		System.out.println("Please select an option from the below utilities: \n1.Banking \n2.to be developed Dude");
+		BufferedReader bufferedReaderOption = new BufferedReader(new InputStreamReader(System.in));
+		option=Integer.valueOf(bufferedReaderOption.readLine()).intValue();
+		System.out.println(option);
+		switch(option)
+		{
+			case 1: System.out.println("Please select the bank");
+					BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+					String bank=bufferedReader.readLine();
+					GetBalanceFromBank getbalance = new GetBalanceFromBank();
+					getbalance.getBalance(bank);
+					break;
+			
+			case 2: System.out.println("u have selected two");
+					break;
+			
+			default:System.out.println("Please select a valid option");
+					break;
+		}
+	}
+}
---
+++ Selenium/utilities/src/dailylife/utilities/properties/SetGeckoDriver.java
@@ -0,0 +1,10 @@
+package dailylife.utilities.properties;
+
+public class SetGeckoDriver 
+{
+	public void geckoDriver()
+	{
+		System.setProperty("webdriver.gecko.driver","D:\\Office\\ForCareer\\Selenium\\geckodriver-v0.10.0-win64\\geckodriver.exe");
+	}
+
+}
