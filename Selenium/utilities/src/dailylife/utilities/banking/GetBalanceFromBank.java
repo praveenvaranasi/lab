d944a559fba546bd46a4e21f829a90f1e27d508a
Praveen <praveenb.varanasi@outlook.com>
2016-10-13 01:39:46 +0530

    Started HDFCC banking automation work and completed till login

--- Selenium/utilities/src/dailylife/utilities/banking/GetBalanceFromBank.java
+++ Selenium/utilities/src/dailylife/utilities/banking/GetBalanceFromBank.java
@@ -2,10 +2,14 @@ package dailylife.utilities.banking;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.server.handler.FindElement;
+
 import dailylife.utilities.properties.SetGeckoDriver;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.io.IOException;
 
 
@@ -21,14 +25,14 @@ public class GetBalanceFromBank
 		gecDrive.geckoDriver();
 		String balance="null";
 		String bank=bankName;
-		if(bank.equals("sbi"))
+		if(bank.equalsIgnoreCase("sbi"))
 		{
 			System.out.println("Enter the Username of your "+bank+" account");
-			BufferedReader sbiusernameBufferedReader = new BufferedReader(new InputStreamReader(System.in));
-			username = sbiusernameBufferedReader.readLine() ;
+			BufferedReader sbiUsernameBufferedReader = new BufferedReader(new InputStreamReader(System.in));
+			username = sbiUsernameBufferedReader.readLine() ;
 			System.out.println("Enter the password of your "+bank+" account");
-			BufferedReader sbipasswordBufferedReader = new BufferedReader(new InputStreamReader(System.in));
-			password = sbipasswordBufferedReader.readLine() ;
+			BufferedReader sbiPasswordBufferedReader = new BufferedReader(new InputStreamReader(System.in));
+			password = sbiPasswordBufferedReader.readLine() ;
 			WebDriver driver=new FirefoxDriver();
 			driver.navigate().to(sbiurl);
 			driver.manage().window().maximize();
@@ -47,9 +51,37 @@ public class GetBalanceFromBank
 			System.out.println("Balance in your "+bank+" account is "+balance);
 			driver.findElement(By.xpath(".//*[@id='tblContainer']/tbody/tr/td/table[2]/tbody/tr/td[9]/span/a")).click();
 		}
+		else if (bank.equalsIgnoreCase("hdfc"))
+		{
+			System.out.println("Enter the UserID/CustomerID of "+bank+" bank");
+			String userID;
+			BufferedReader bufferedReaderUserID = new BufferedReader(new InputStreamReader(System.in));
+			userID=bufferedReaderUserID.readLine();
+			System.out.println("Enter the Password of UserID of "+bank+" bank");
+			BufferedReader bufferedReaderPassword=new BufferedReader(new InputStreamReader(System.in));
+			password=bufferedReaderPassword.readLine();
+			WebDriver driver = new FirefoxDriver();
+			driver.navigate().to(hdfcurl);
+			driver.manage().window().maximize();
+			Thread.sleep(5000);
+			driver.switchTo().frame("login_page");
+			driver.findElement(By.className("pwd_field")).sendKeys(userID);
+			Thread.sleep(3000);
+			driver.findElement(By.xpath("html/body/form/table[2]/tbody/tr/td[2]/table/tbody/tr[1]/td[1]/table/tbody/tr[3]/td[2]/table/tbody/tr[6]/td[2]/a/img")).click();
+			Thread.sleep(5000);
+			driver.findElement(By.className("pwd_field")).sendKeys(password);
+			driver.findElement(By.name("chkrsastu")).click();
+			Thread.sleep(5000);
+			driver.findElement(By.xpath("html/body/form/table[2]/tbody/tr/td[2]/table/tbody/tr[1]/td[1]/table/tbody/tr[3]/td[2]/table/tbody/tr[6]/td/table/tbody/tr[9]/td/a/img")).click();
+			Thread.sleep(4000);
+			driver.findElement(By.id(".//*[@id='savingAcctList']")).click();
+			Thread.sleep(4000);
+			balance=driver.findElement(By.id(".//*[@id='SavingTotalSummary']")).getText();
+			System.out.println("Balance in your "+bank+" account is "+balance);
+		}
 		else
 		{
-			System.out.println("I cant process your request. Right now, I can give balance only");
+			System.out.println("Select among the hdfc and sbi banks");
 		}
 	}
 }
