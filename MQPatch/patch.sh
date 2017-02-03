f0e3c4a6acd78775b1243f4c93da053416628c9d
Praveen <praveenb.varanasi@outlook.com>
2017-02-03 14:48:42 +0530

    made changes

--- MQPatch/patch.sh
+++ MQPatch/patch.sh
@@ -1,1 +1,14 @@
 #! /bin/sh
+
+#Checking directory existence
+
+workingDirectory=$(pwd);
+echo $workingDirectory;
+checkDir=$1;
+if [ ! -d $1];
+then
+	echo "not a directory";
+else
+	echo "directory";
+fi
+
